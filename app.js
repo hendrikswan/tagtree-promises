19aae9f2b7b10691bc063f77059558e02b03d3cf
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-05-16 20:26:13 +0200

    pyramid

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 function generateRandomNumber(cb){
     setTimeout(function(){
         cb(Math.floor(Math.random() * 100));
-    }, 500);
+    }, 50);
 }
 
 
-generateRandomNumber(function(nr){
-    console.log('the  number: ', nr);
+generateRandomNumber(function(nr1){
+    generateRandomNumber(function(nr2){
+        generateRandomNumber(function(nr3){
+            generateRandomNumber(function(nr4){
+                generateRandomNumber(function(nr5){
+                    generateRandomNumber(function(nr6){
+                        generateRandomNumber(function(nr7){
+                            console.log(nr7 + nr6 + nr5 + nr4 + nr3 + nr2 + nr1);
+                        });
+                    });
+                });
+            });
+        });
+    });
 });
