7e5798e17d358c318acac3ea2b2dc0dfdc47ece1
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-05-16 20:46:12 +0200

    nexted promises

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
+var Q = require('q');
+
 function generateRandomNumber(cb){
     setTimeout(function(){
-        cb(Math.floor(Math.random() * 100));
+        cb(null, Math.floor(Math.random() * 100));
     }, 50);
 }
 
+var genPromise = Q.denodeify(generateRandomNumber);
 
-generateRandomNumber(function(nr1){
-    generateRandomNumber(function(nr2){
-        generateRandomNumber(function(nr3){
-            generateRandomNumber(function(nr4){
-                generateRandomNumber(function(nr5){
-                    generateRandomNumber(function(nr6){
-                        generateRandomNumber(function(nr7){
-                            console.log(nr7 + nr6 + nr5 + nr4 + nr3 + nr2 + nr1);
-                        });
-                    });
-                });
-            });
+genPromise().then(function(nr1){
+    genPromise().then(function(nr2){
+        genPromise().then(function(nr3){
+            console.log(nr1 + nr2 + nr3);
         });
     });
 });
+
+
+
+
+// generateRandomNumber(function(nr1){
+//     generateRandomNumber(function(nr2){
+//         generateRandomNumber(function(nr3){
+//             generateRandomNumber(function(nr4){
+//                 generateRandomNumber(function(nr5){
+//                     generateRandomNumber(function(nr6){
+//                         generateRandomNumber(function(nr7){
+//                             console.log(nr7 + nr6 + nr5 + nr4 + nr3 + nr2 + nr1);
+//                         });
+//                     });
+//                 });
+//             });
+//         });
+//     });
+// });
