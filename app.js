a4cfd3129427416f0c742f7484a1bad9a66a3ee9
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-06-21 15:02:33 +0200

    final code

--- app.js
+++ app.js
@@ -21,22 +21,3 @@ genPromise(0)
     .then(function(total){
         console.log(total);
     });
-
-
-
-
-// generateRandomNumber(function(nr1){
-//     generateRandomNumber(function(nr2){
-//         generateRandomNumber(function(nr3){
-//             generateRandomNumber(function(nr4){
-//                 generateRandomNumber(function(nr5){
-//                     generateRandomNumber(function(nr6){
-//                         generateRandomNumber(function(nr7){
-//                             console.log(nr7 + nr6 + nr5 + nr4 + nr3 + nr2 + nr1);
-//                         });
-//                     });
-//                 });
-//             });
-//         });
-//     });
-// });
